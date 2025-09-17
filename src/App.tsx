e1bd392bc1fda11e592208727e8851367c8bbb69
Justin GALLY <justin@jgcoding.com>
2025-09-17 21:50:36 +0200

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Click me {count}
+      </Button>
+    </>
+  );
+}
+
+export default App;
